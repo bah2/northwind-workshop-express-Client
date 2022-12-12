a98660fbb74c0822ca587f9350fb53e4d1e031c7
Ibrahim <ibrahimbahraj@gmail.com>
2022-12-12 09:09:45 -0500

    minor changes

--- scripts/details.js
+++ scripts/details.js
@@ -1,1 +1,27 @@
-//  come to this later and update
+"use strict";
+
+const tableBody = document.getElementById("tableBody");
+window.onload = () => {
+  const linkPr = new URLSearchParams(location.search);
+  let id = -1;
+
+  if (linkPr.has("productId") == true) {
+    id = linkPr.get("productId");
+
+    fetch(`http://localhost:8081/api/products/${id}`)
+      .then((Response) => Response.json())
+      .then((data) => {
+        // if(1 == products.categoryId){
+        let row = tableBody.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+
+        cell1.innerHTML = `${data.productName} ${data.productName}`;
+        cell2.innerHTML = `$${parseFloat(data.unitPrice).toFixed(2)}`;
+        cell3.innerHTML = data.categoryId;
+        cell4.innerHTML = data.productId;
+      });
+  }
+};
--- scripts/productSearch.js
+++ scripts/productSearch.js
@@ -3,16 +3,28 @@ const mainDropdown = document.getElementById("mainDropdown");
 const Searchbycategory = document.getElementById("Searchbycategory");
 const viewall = document.getElementById("viewall");
 const SearchProductBycategory = document.getElementById(
-  "SearchProductBycategory");
-  const tableHead = document.getElementById("tableHead");
- document.getElementById("SearchProductBycategory").style.display = "none"
-//  document.getElementById("tableHead").style.display = "none"
-
+  "SearchProductBycategory"
+);
+const tableHead = document.getElementById("tableHead");
+document.getElementById("SearchProductBycategory").style.display = "none";
+const entireTable = document.getElementById("entireTable");
 
 window.onload = () => {
-  mainDropdown.onchange = SearchByCategoryDd;
+  function displayData() {
+    let row = tableBody.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
 
- 
+    cell1.innerHTML = `<a href = "details.html?productId=${products.productName}"> ${products.productName}</a>`;
+    cell2.innerHTML = `$${parseFloat(products.unitPrice).toFixed(2)}`;
+    cell3.innerHTML = products.categoryId;
+    cell4.innerHTML = products.productId;
+  }
+
+  mainDropdown.onchange = SearchByCategoryDd;
+  clearField(tableBody);
 
   function SearchByCategoryDd() {
     SearchProductBycategory.length = 0;
@@ -30,7 +42,8 @@ window.onload = () => {
 
     mainDropdown.onchange = () => {
       clearField(tableBody);
-      document.getElementById("SearchProductBycategory").style.display = "block"
+      document.getElementById("SearchProductBycategory").style.display =
+        "block";
       // document.getElementById("tableHead").style.display = "block"
 
       if (mainDropdown.value == "viewall") {
@@ -42,8 +55,6 @@ window.onload = () => {
       }
 
       function productDisplay(data) {
-        clearField(tableBody);
-
         for (let products of data) {
           let row = tableBody.insertRow(-1);
           let cell1 = row.insertCell(0);
@@ -52,7 +63,7 @@ window.onload = () => {
           let cell4 = row.insertCell(3);
 
           cell1.innerHTML = products.productName;
-          cell2.innerHTML = products.unitPrice;
+          cell2.innerHTML = `$${parseFloat(products.unitPrice).toFixed(2)}`;
           cell3.innerHTML = products.categoryId;
           cell4.innerHTML = products.productId;
         }
@@ -61,6 +72,7 @@ window.onload = () => {
   }
 
   SearchProductBycategory.onchange = () => {
+    clearField(tableBody);
     fetch(`http://localhost:8081/api/products`)
       .then((Response) => Response.json())
       .then((data) => {
@@ -76,12 +88,11 @@ window.onload = () => {
           let cell3 = row.insertCell(2);
           let cell4 = row.insertCell(3);
 
-          cell1.innerHTML = products.productName;
-          cell2.innerHTML = products.unitPrice;
+          cell1.innerHTML = `<a href = "details.html?productId=${products.productId}"> ${products.productName}</a>`;
+          cell2.innerHTML = `$${parseFloat(products.unitPrice).toFixed(2)}`;
           cell3.innerHTML = products.categoryId;
           cell4.innerHTML = products.productId;
         }
-
       }
     }
   };
